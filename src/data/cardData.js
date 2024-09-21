38c52570accad0a2217619d24ccef8880a39f3a8
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-09-21 12:36:08 +0200

    Add myFlix API  project

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -27,7 +27,7 @@ const cardData = [
   {
     title: 'Meet App',
     img: meet,
-    text: 'React SPA built using TDD and BDD approaches. Jest, Ghekins, Puppeteer and Jest-Cucumber used for tesing. Atatus is used to experiment app performance monitoring.  The Backend (Server Logic): Was written with Node/Express as Lambda functions (FaaS); hosted on AWS (requests come from frontend to Lambda function to data). While the Backend (Database): A Google Calendar API. This app allows users to search for upcoming events in their city and view the events on a map.',
+    text: 'React SPA built using TDD and BDD approaches. Jest, Ghekins, Puppeteer and Jest-Cucumber used for tesing. Atatus is used to experiment app performance monitoring.  The Backend (Server Logic): Was written with Node/Express as Lambda functions; hosted on AWS. While the Backend (Database): is a Google Calendar API. This app allows users to search for upcoming events in their city.',
     appLink: 'https://nodem9.github.io/meet/',
     codeLink: 'https://github.com/NodEm9/mee'
   },
