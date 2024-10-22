ed5db5f4e06f58a147bc1e224db610938eae4e60
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-10-22 07:44:29 +0200

    Remove normal method and change app to a SPA

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import './App.css';
-import { Routes, Route, HashRouter } from 'react-router-dom';
 import WelcomePage from './components/welcome-view/welcome-view';
 import Navigation from './components/navigation-view/navigation';
 import Footer from './components/footer-view/footer-view';
-import About from './components/about-view/about-view';
-import Work from './components/work-view/work-view';
-import Contact from './components/contact-view/contact-view';
-import PostsView from './components/blog-posts/postOne';
+
 
 
 function App() {
   return (
-    <>
-      <HashRouter>
+    <main>
         <Navigation />
-        <Routes>
-          <Route path="/" exact element={<WelcomePage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blog" element={<PostsView />} />
-        </Routes>
+        <WelcomePage />
         <Footer />
-      </HashRouter>
-    </>
+    </main>
   );
 }
 
